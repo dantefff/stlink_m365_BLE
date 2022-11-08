c53dc0e190d974c6bdcbb6d4193d0ec457296d58
Iago <ifmeilan@acore.es>
2022-11-08 14:32:01 +0100

    Make ReprogramProPurple.sh a bash script

--- flashing/ReprogramProPurple.sh
+++ flashing/ReprogramProPurple.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/bash
 echo "you have 10 seconds to connect wires"
 sleep 10
 openocd -f ../scripts/interface/stlink-v2.cfg -c "transport select hla_swd" -f ../scripts/target/nrf51.cfg -c "init" -c "reset halt" -c "nrf51 mass_erase" -c "program pruple_pro074_flash.hex verify" -c "reset run" -c "exit"
